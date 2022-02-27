0b54b367bdd321a54f149d479166442e280464c4
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-02-27 07:37:23 -0500

    Finished creating thresholdReached method. Test passed.

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -4,7 +4,8 @@ import java.util.logging.Logger;
 public class LogginLab {
     private final static Logger logger = Logger.getLogger(LogginLab.class.getName());
 
-    private Integer threshold = 0;
+    private  static Integer threshold = 0;
+
 
     public LogginLab() {
         this.threshold = 0;
@@ -12,26 +13,28 @@ public class LogginLab {
 
     public static void main(String[] args) {
 
-        logger.log(Level.INFO, "Hello World!");
-        logger.log(Level.SEVERE, "Terrible Error!");
-        logger.log(Level.WARNING, "Not So Bad Error!");
+            logger.log(Level.INFO, "Hello World!");
+            logger.log(Level.SEVERE, "Terrible Error!");
+            logger.log(Level.WARNING, "Not So Bad Error!");
 
-        logger.log(Level.INFO, "****\n\tAt ZipCode, \n\twe don't use System.out.Println \n\tuntil we've earned the right.\n****");
+            logger.log(Level.INFO, "****\n\tAt ZipCode, \n\twe don't use System.out.Println \n\tuntil we've earned the right.\n****");
 
-    }
+        }
 
-    public Integer getThreshold() {
-        return threshold;
-    }
+        public Integer getThreshold () {
+            return threshold;
+        }
 
-    public void setThreshold(Integer threshold) {
-        this.threshold = threshold;
-    }
+        public void setThreshold (Integer threshold){
+            this.threshold = threshold;
+        }
 
-    public boolean thresholdExceeds(Integer limit) {
-        return (this.threshold > limit);
-    }
+        public boolean thresholdExceeds (Integer limit){
+            return (this.threshold > limit);
+        }
 
-    // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
-    // Write a test for the method in the Test class.
-}
+        }
+        // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
+        // Write a test for the method in the Test class.
+
+
